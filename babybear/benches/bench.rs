a2fab0a0565cffd514f15e552a36b67fbe3b705e
zhenfei <zhenfei.zhang@hotmail.com>
2024-06-25 22:25:50 -0400

    bench dft

--- babybear/benches/bench.rs
+++ babybear/benches/bench.rs
@@ -3,6 +3,8 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use ff::Field;
 use goldilocks::SmallField;
 use halo2curves::bn256::Fr;
+use p3_dft::TwoAdicSubgroupDft;
+use p3_field::TwoAdicField;
 use rand_core::SeedableRng;
 use rand_xorshift::XorShiftRng;
 
@@ -13,6 +15,7 @@ criterion_main!(bench);
 criterion_group!(bench, bench_fields);
 
 fn bench_fields(c: &mut Criterion) {
+    bench_dft::<Babybear>(c, Babybear::NAME);
     bench_field::<Babybear>(c, Babybear::NAME);
     bench_field::<BabybearExt3>(c, BabybearExt3::NAME);
     bench_field::<Fr>(c, "Bn256 scalar")
@@ -47,5 +50,25 @@ fn bench_field<F: Field>(c: &mut Criterion, field_name: &str) {
                 .collect::<Vec<_>>()
         })
     });
+}
+
+fn bench_dft<F: Field + TwoAdicField>(c: &mut Criterion, field_name: &str) {
+    let mut bench_group = c.benchmark_group(format!("{}", field_name));
+
+    let mut rng = XorShiftRng::from_seed([
+        0x59, 0x62, 0xbe, 0x5d, 0x76, 0x3d, 0x31, 0x8d, 0x17, 0xdb, 0x37, 0x32, 0x54, 0x06, 0xbc,
+        0xe5,
+    ]);
+    for size in 10..20 {
+        let coeffs = (0..(1 << size))
+            .map(|_| F::random(&mut rng))
+            .collect::<Vec<_>>();
+
+        let bench_str = format!("dim {} dft", 1 << size);
+        bench_group.bench_function(bench_str, |bencher| {
+            bencher.iter(|| p3_dft::Radix2DitParallel.dft(coeffs.clone()))
+        });
+    }
+
     bench_group.finish();
 }
--- goldilocks/benches/bench.rs
+++ goldilocks/benches/bench.rs
@@ -2,6 +2,8 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use ff::Field;
 use goldilocks::{Goldilocks, GoldilocksExt2, GoldilocksExt3, SmallField};
 use halo2curves::bn256::Fr;
+use p3_dft::TwoAdicSubgroupDft;
+use p3_field::TwoAdicField;
 use rand_core::SeedableRng;
 use rand_xorshift::XorShiftRng;
 
@@ -12,6 +14,7 @@ criterion_main!(bench);
 criterion_group!(bench, bench_fields);
 
 fn bench_fields(c: &mut Criterion) {
+    bench_dft::<Goldilocks>(c, Goldilocks::NAME);
     bench_field::<Goldilocks>(c, Goldilocks::NAME);
     bench_field::<GoldilocksExt2>(c, GoldilocksExt2::NAME);
     bench_field::<GoldilocksExt3>(c, GoldilocksExt3::NAME);
@@ -47,5 +50,28 @@ fn bench_field<F: Field>(c: &mut Criterion, field_name: &str) {
                 .collect::<Vec<_>>()
         })
     });
+    bench_group.finish();
+}
+
+
+
+fn bench_dft<F: Field + TwoAdicField>(c: &mut Criterion, field_name: &str) {
+    let mut bench_group = c.benchmark_group(format!("{}", field_name));
+
+    let mut rng = XorShiftRng::from_seed([
+        0x59, 0x62, 0xbe, 0x5d, 0x76, 0x3d, 0x31, 0x8d, 0x17, 0xdb, 0x37, 0x32, 0x54, 0x06, 0xbc,
+        0xe5,
+    ]);
+    for size in 10..20 {
+        let coeffs = (0..(1 << size))
+            .map(|_| F::random(&mut rng))
+            .collect::<Vec<_>>();
+
+        let bench_str = format!("dim {} dft", 1 << size);
+        bench_group.bench_function(bench_str, |bencher| {
+            bencher.iter(|| p3_dft::Radix2DitParallel.dft(coeffs.clone()))
+        });
+    }
+
     bench_group.finish();
 }
